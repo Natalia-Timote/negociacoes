a553db22dddbfeb8ca5f95ce084b6956dbc57dcc
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-07-09 22:30:11 -0300

    first commit + ts

---
+++ app/app.ts
@@ -0,0 +1,4 @@
+import { Negociacao } from "../dist/js/models/negociacao.js";
+
+const negociacao = new Negociacao(new Date, 10, 100);
+console.log(negociacao);
---
+++ dist/js/app.js
@@ -0,0 +1,3 @@
+import { Negociacao } from "../dist/js/models/negociacao.js";
+const negociacao = new Negociacao(new Date, 10, 100);
+console.log(negociacao);
